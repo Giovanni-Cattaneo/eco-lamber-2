e03297f74d8db0a101ba6d284554d51591e61b48
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-05-29 22:21:33 +0200

    add partial and guest views

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-}); //http://localhost:8000/
+})->name('home'); //http://localhost:8000/
 
 
 Route::middleware(['auth', 'verified'])
